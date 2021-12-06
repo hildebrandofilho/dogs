ac0b9ed890769bd07d51f767269051e9b2941e01
Hildebrando dos Santos Filho <83103911+hildebrandofilho@users.noreply.github.com>
2021-12-06 10:45:31 -0300

    add projeto
    add projeto

---
+++ src/Components/User/User.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Routes } from 'react-router';
+import { UserContext } from '../../UserContext';
+import Feed from '../Feed/Feed';
+import Head from '../Helper/Head';
+import UserHeader from './UserHeader';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
+
+const User = () => {
+  const { data } = React.useContext(UserContext);
+
+  return (
+    <section className="container">
+      <Head title="Minha Conta" />
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed user={data.id} />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+      </Routes>
+    </section>
+  )
+}
+
+export default User
